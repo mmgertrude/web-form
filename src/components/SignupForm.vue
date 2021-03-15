cd919674f3e85916cf27adbc0d57a2f247b29479
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2021-03-15 21:30:55 +0100

    first commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,148 @@
+<template>
+  <form @submit.prevent="handleFormSubmission">
+      <label>Email:</label>
+      <input type="email" required v.model="email">
+
+      <label>Password:</label>
+      <input type="password" required v.model="password">
+      <!-- passwordError is false if passwordError="" -->
+      <div v-if="passwordError" class="error">{{ passwordError}}</div>
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer"> Web Developer</option>
+          <option value="designer"> Web Designer</option>
+      </select>
+
+      <div class="terms">
+          <input type="checkbox" v-model="terms" required>
+          <label>Accept terms and conditions</label>
+      </div>
+
+      <label>Skills</label>
+      <input type="text" v-model="tempSkill" @keyup="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill">
+          <span @click="removeSkill(skill)"> {{skill}}</span>
+      </div>
+
+      <div class="submit">
+          <button>Create an Account</button>
+      </div>
+
+
+  </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+        email: "",
+        password: "",
+        role: "designer",
+        terms: false,
+        tempSkill: "",
+        skills: [],
+        passwordError: ""
+        }
+    },
+    methods:{
+        addSkill(e){
+            if(e.key==="Enter" && this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ""
+            }
+        },
+        removeSkill(clickedSkill){
+            this.skills = this.skills.filter((item) =>{
+                return clickedSkill !== item
+            })
+        },
+        handleFormSubmission(){
+            //validate pwd using the ternary operator
+            this.passwordError = this.passwordError.length > 5 ? 
+            "" : "Password must be atleast 6 chars long"
+           if(!this.passwordError){
+               //normally you would submit to a DB
+               //but i am logging to the console:
+               console.log("email: ", this.email)
+               console.log("password: ", this.password)
+               console.log("role: ", this.role)
+               console.log("skills: ", this.skills)
+               console.log("terms accepted: ", this.terms)
+
+
+
+           } 
+        }
+    }
+
+}
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label{
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input, select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border:none;
+    border-bottom: 1px solid#ddd;
+    color: #555;
+}
+input[type="checkbox"]{
+    display: inline-block; /*so it sits next to the lable */
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+.pill{
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+button{
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit{
+    text-align: center;
+}
+.error{
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+}
+</style>
